97c2acbfa498d476938004909b6377ceb212f854
wolfgray <5565486@mail.ru>
2023-01-07 13:09:09 +0300

    finaly

--- mail.php
+++ mail.php
@@ -3,13 +3,13 @@
 //Пример создан сайтом http://blogjquery.ru/ajax-rdmailform-forma-otpravki-soobshhenij-bez-perezagruzki-stranicy/
 
 //Первая форма
-if(isset($_POST['name']) && !empty($_POST['email'])) {
-$to = '5565426@mail.ru';
+if(isset($_POST['name']) && !empty($_POST['name'])) {
+$to = 'step2nereality@gmail.com';
 $subject = 'Сообщение с сайта '.$_SERVER['SERVER_NAME'];
 $charset = "utf-8";
 $headerss ="Content-type: text/html; charset=$charset\r\n";
 $headerss.="Date: ".date('D, d M Y h:i:s')."\r\n";
-$msgotprav = 'Ваше имя: '.$_POST['name'].'<br>Ваш Email: '.$_POST['email'].'<br>Текст сообщения: '.$_POST['message'];
+$msgotprav = 'Имя заказчика: '.$_POST['name'].'<br>Телефон заказчика: '.$_POST['phone'];
 $msgotprav .= '<br><br>Отправлено со страницы: '.$_SERVER['HTTP_REFERER'];
 mail($to, $subject, $msgotprav, $headerss);
 echo 'MF000'; //если сообщение отправлено ajax-ом - выводим статус успешной доставки
--- script.js
+++ script.js
@@ -6,6 +6,29 @@ let modalMenu = document.querySelector(".modal-menu");
 let modalForm = document.querySelector(".modal-form");
 let sendBtn = document.querySelectorAll(".close-form-after-send");
 let closeBtn = document.querySelector(".hide-btn");
+let container = document.querySelector(".game-list");
+const games = [
+  "img/games/scenariusz-echo-vr.jpg",
+  "img/games/scenariusz-dead-n-buried.jpg",
+  "img/games/scenariusz-boxvr.jpg",
+  "img/games/scenariusz-arizona-sunshine.jpg",
+  "img/games/scenariusz_robo_recall.jpg",
+  "img/games/scenariusz_elven-assasin.jpg",
+  "img/games/scenariusz_beat_saber.jpg",
+  "img/games/gra-vader-immortal-1.jpg",
+  "img/games/gra-the-climb.jpg",
+  "img/games/gra-rec-room.jpg",
+  "img/games/gra-luckys-tale.jpg",
+  "img/games/gra-lone-echo.jpg"
+];
+
+for (let el of games) {
+  console.log(typeof el);
+  let item = document.createElement("img");
+  item.className = 'game-item'
+  item.src = el;
+  container.append(item);
+}
 
 const modalAct = () => {
   modalForm.classList.toggle("hide");
