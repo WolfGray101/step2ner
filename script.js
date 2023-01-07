208b030d83534b4546eeccbb8bbd0914353f3502
wolfgray <5565486@mail.ru>
2023-01-07 03:40:36 +0300

    add feedback form

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
 
 let showModal = document.querySelector('.menu-button-burger');
+let showModalForm = document.querySelectorAll('.feedback-block');
+// let formShow = document.querySelector('.form-show')
 let body = document.querySelector('body');
 let dFix = document.querySelector('.d-fix');
-
 let modalMenu = document.querySelector('.modal-menu');
-// let selectMenu = document.querySelectorAll('.modal-menu');
-
-// for (let listElem of selectMenu) {
-//   listElem.onclick = function () {
-//     body.classList.toggle('d-fix');
-//     showAll.classList.toggle('rotate-burger');
-//     for (let elem of cardItems) {
-//       elem.classList.toggle('modal-menu');
-//       elem.classList.toggle('show');
-//     }
-//   };  
-// }
-
-
-console.log(modalMenu.childNodes);
-
+let modalForm = document.querySelector('.modal-form');
+let sendBtn = document.querySelectorAll('.btn-mailform')
+let closeBtn = document.querySelector('.close')
 
   showModal.addEventListener('click' , () => {
-  showModal.classList.toggle('rotate-burger');
-  modalMenu.classList.toggle('hide');
-
-  modalMenu.classList.toggle('show');
-  body.classList.toggle('d-fix');
-
+    modalMenuAct()
 });
-
 
 for (let elem of modalMenu.childNodes) {
   console.log(elem);
   elem.addEventListener('click' , () => {
-    showModal.classList.toggle('rotate-burger');
-    modalMenu.classList.toggle('hide');    
-    modalMenu.classList.toggle('show');
-    body.classList.toggle('d-fix');
+    modalMenuAct()
   })
-
 }
 
-// let card = document.querySelector('.scheme-card');
+for (let elem of showModalForm) {
+  elem.addEventListener('click' , () => {
+    ModalAct()
+  })
+}
 
-// let cardsFront = card.querySelector('.front');
-// let cardsBack = card.querySelector('.back');
+for (let el of sendBtn) {
+  el.addEventListener('click', () => {
+    ModalAct()
+  })
+}
 
-// card.addEventListener( 'click', () => {
-//  cardsFront.classList.toggle('scheme-card-rotate-front');
-//  cardsBack.classList.toggle('scheme-card-rotate-back');
-// }
-// )
+closeBtn.addEventListener('click' , () => {
+  console.log('hello');
+  ModalAct()
 
+})
+
+const ModalAct = () => {
+  modalForm.classList.toggle('hide');    
+    modalForm.classList.toggle('form-show');
+    body.classList.toggle('d-fix'); 
+}
+
+const modalMenuAct = () => {
+  showModal.classList.toggle('rotate-burger');
+  modalMenu.classList.toggle('hide');    
+  modalMenu.classList.toggle('show');
+  body.classList.toggle('d-fix');
+}
 
