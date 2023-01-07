e318f885b048e1bebe2d3fb41c1fa82acb8b4977
wolfgray <5565486@mail.ru>
2023-01-07 04:53:08 +0300

    add contact links

--- script.js
+++ script.js
@@ -1,59 +1,49 @@
-
-let showModal = document.querySelector('.menu-button-burger');
-let showModalForm = document.querySelectorAll('.feedback-block');
-// let formShow = document.querySelector('.form-show')
-let body = document.querySelector('body');
-let dFix = document.querySelector('.d-fix');
-let modalMenu = document.querySelector('.modal-menu');
-let modalForm = document.querySelector('.modal-form');
-let sendBtn = document.querySelectorAll('.btn-mailform')
-let closeBtn = document.querySelector('.hide-btn')
-
-
-
+let showModal = document.querySelector(".menu-button-burger");
+let showModalForm = document.querySelectorAll(".feedback-block");
+let body = document.querySelector("body");
+let dFix = document.querySelector(".d-fix");
+let modalMenu = document.querySelector(".modal-menu");
+let modalForm = document.querySelector(".modal-form");
+let sendBtn = document.querySelectorAll(".btn-mailform");
+let closeBtn = document.querySelector(".hide-btn");
 
 const modalAct = () => {
-  modalForm.classList.toggle('hide');    
-    modalForm.classList.toggle('form-show');
-    body.classList.toggle('d-fix'); 
-}
+  modalForm.classList.toggle("hide");
+  modalForm.classList.toggle("form-show");
+  body.classList.toggle("d-fix");
+};
 
 const modalMenuAct = () => {
-  showModal.classList.toggle('rotate-burger');
-  modalMenu.classList.toggle('hide');    
-  modalMenu.classList.toggle('show');
-  body.classList.toggle('d-fix');
-}
+  showModal.classList.toggle("rotate-burger");
+  modalMenu.classList.toggle("hide");
+  modalMenu.classList.toggle("show");
+  body.classList.toggle("d-fix");
+};
 
-
-showModal.addEventListener('click' , () => {
-  modalMenuAct()
+showModal.addEventListener("click", () => {
+  modalMenuAct();
 });
 
 for (let elem of modalMenu.childNodes) {
   console.log(elem);
-  elem.addEventListener('click' , () => {
-    modalMenuAct()
-  })
+  elem.addEventListener("click", () => {
+    modalMenuAct();
+  });
 }
 
 for (let elem of showModalForm) {
-  elem.addEventListener('click' , () => {
-    modalAct()
-  })
+  elem.addEventListener("click", () => {
+    modalAct();
+  });
 }
 
 for (let el of sendBtn) {
-  el.addEventListener('click', () => {
-    modalAct()
-  })
+  el.addEventListener("click", () => {
+    modalAct();
+  });
 }
 
-closeBtn.addEventListener('click' , () => {
-  console.log('hello');
-  modalAct()
-
-})
-
-
-
+closeBtn.addEventListener("click", () => {
+  console.log("hello");
+  modalAct();
+});
